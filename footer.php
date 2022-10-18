8366d5d9c10f9b5b6c3d978ca44c4e83d74fdaae
Mikhail Trefilov <67998172+MIke-Freezing@users.noreply.github.com>
2022-10-18 17:20:12 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,1 +1,14 @@
-<?php
+<footer>
+    <nav>
+        <p>
+            <a href="index.php?page=<? echo $page ?>&css=css1&titre=<? echo $titre ?>">
+            CSS 1</a>
+        </p>
+        <p>
+            <a href="index.php?page=<? echo $page ?>&css=css2&titre=<? echo $titre ?>">
+            CSS 2</a>
+        </p>
+    </nav>
+</footer>
+</body>
+</html>
