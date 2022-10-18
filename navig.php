8c719f637fb8738159ded8a820ce37ac5bdc97f8
Mikhail Trefilov <67998172+MIke-Freezing@users.noreply.github.com>
2022-10-18 17:59:05 +0200

    Update navig.php
    bruillon nav

--- navig.php
+++ navig.php
@@ -1,1 +1,9 @@
-<?php
+<nav>
+  <? 
+    echo '<p><a href="index.php?page=home&css='.$css.'&titre=introduction"introduction</a></p'>;
+    echo '<p><a href="index.php?page=page_index&css='.$css.'&titre=moteur"moteur</a></p'>;
+    echo '<p><a href="index.php?page=page_haut&css='.$css.'&titre=haut de page"haut de page</a></p'>;
+    echo '<p><a href="index.php?page=page_navig&css='.$css.'&titre=navigation"navigation</a></p'>;
+    echo '<p><a href="index.php?page=page_bas&css='.$css.'&titre=pied de page"pied de page</a></p'>;
+  ?>
+</nav>
